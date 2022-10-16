1c29435b5194f0254f2d0806da344601d3ff417e
Nick Nguyen <nick@Navenu.com>
2022-10-16 00:33:51 -0700

    update

--- app/interface/feed.ts
+++ app/interface/feed.ts
@@ -1,9 +1,21 @@
 
 export interface Feed{
-    type: 'article' 
-    id: number,
-    category: string,
-    tags: string,
-    name: string,
-    description: string,
+    type: 'Article' | 'location' | 'drop'
+    id: number
+    name: string
+    lat:number
+            lng:number
+            image:string
+            phone: number
+            parent_category:string
+            category:string
+            distance: number  //Below are for articles and drops
+            intro:string
+            description:string
+            owner:string
+            tags:string
+            venue:string   //Below are for drops only
+            venue_id:number
+            expiration:Date
+
 }
