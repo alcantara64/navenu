dcc101980f6c4718d6a6d83c53988581ae933e43
Emmanuel Agahiu <emmanuelagahiu@gmail.com>
2022-11-14 07:51:55 +0100

    fix: remove console logs

--- app/app.tsx
+++ app/app.tsx
@@ -66,10 +66,8 @@ function App(props: AppProps) {
     return notifee.onForegroundEvent(({ type, detail }) => {
       switch (type) {
         case EventType.DISMISSED:
-          console.log('User dismissed notification', detail.notification);
           break;
         case EventType.PRESS:
-          console.log('User pressed notification', detail.notification);
           break;
       }
     });
