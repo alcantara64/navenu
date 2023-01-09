49f315561d9875b298b2874194324c96fb22ddfb
Ariel Conti <arielconti10@gmail.com>
2023-01-09 20:45:27 +0000

    Google signin integration + apple signin

--- app/screens/SignUp/SignUpStartScreen.tsx
+++ app/screens/SignUp/SignUpStartScreen.tsx
@@ -1,6 +1,6 @@
 import { observer } from "mobx-react-lite"
 import React, { FC } from "react"
-import { ImageBackground, View, Dimensions, ViewStyle, TextStyle } from "react-native"
+import { ImageBackground, View, Dimensions, ViewStyle, TextStyle, Alert } from "react-native"
 import { Text } from "react-native-ui-lib"
 import { FontAwesome5 } from "@expo/vector-icons"
 
@@ -13,6 +13,9 @@ import { GOOGLE_ANDROID_CLIENT_ID, GOOGLE_IOS_CLIENT_ID } from "@env"
 
 import * as WebBrowser from "expo-web-browser"
 import * as Google from "expo-auth-session/providers/google"
+import * as AppleAuthentication from "expo-apple-authentication"
+
+import { useStores } from "../../models"
 
 const authImage = require("../../../assets/images/auth/auth-start-image.png")
 
@@ -24,12 +27,15 @@ export const SignUpStartScreen: FC<SignUpStartScreenProps> = observer(function S
   _props,
 ) {
   const navigation = useNavigation()
+  const {
+    authenticationStore: { socialRegister },
+  } = useStores()
+
   const goToFormPage = () => {
     navigation.navigate("SignUpForm")
   }
 
   const [accessToken, setAccessToken] = React.useState(null)
-  const [user, setUser] = React.useState(null)
   const [_request, response, promptAsync] = Google.useAuthRequest({
     iosClientId: GOOGLE_IOS_CLIENT_ID,
     androidClientId: GOOGLE_ANDROID_CLIENT_ID,
@@ -47,11 +53,45 @@ export const SignUpStartScreen: FC<SignUpStartScreenProps> = observer(function S
       headers: { Authorization: `Bearer ${accessToken}` },
     })
     const useInfo = await response.json()
-    setUser(useInfo)
 
-    // todo: send user info to backend and register the user
+    if (useInfo) {
+      socialRegister({
+        email: useInfo.email,
+        firstName: useInfo.given_name,
+        lastName: useInfo.family_name,
+        socialId: useInfo.id,
+        authType: "google",
+        avatar: useInfo.picture,
+      })
+    }
   }
 
+  const handleAppleSignin = async () => {
+    try {
+      const credential = await AppleAuthentication.signInAsync({
+        requestedScopes: [
+          AppleAuthentication.AppleAuthenticationScope.FULL_NAME,
+          AppleAuthentication.AppleAuthenticationScope.EMAIL,
+        ],
+      })
+
+      if (credential) {
+        socialRegister({
+          email: credential.email,
+          firstName: credential.fullName.givenName,
+          lastName: credential.fullName.familyName,
+          socialId: credential.user,
+          authType: "apple",
+        })
+      }
+
+    } catch (e) {
+      console.log(e)
+      if (e.code === "ERR_CANCELED") {
+        Alert.alert('Sign in with Apple canceled')
+      } 
+    }
+  }
 
   return (
     <>
@@ -78,7 +118,7 @@ export const SignUpStartScreen: FC<SignUpStartScreenProps> = observer(function S
               <FontAwesome5 name="google" size={24} color="#FFFFFF" />
               <Text style={$buttonText}>Continue with google</Text>
             </AppButton>
-            <AppButton style={$button}>
+            <AppButton style={$button} onPress={handleAppleSignin}>
               <FontAwesome5 name="apple" size={24} color="#FFFFFF" />
               <Text style={$buttonText}>Continue with Apple</Text>
             </AppButton>
