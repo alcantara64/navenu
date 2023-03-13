23128d80c99e7ddb484f9e6adb8d423b4e156e14
Emmanuel Agahiu <emmanuelagahiu@gmail.com>
2023-03-14 00:11:03 +0100

    fix concise chat

--- app/screens/HomeScreen.tsx
+++ app/screens/HomeScreen.tsx
@@ -1,22 +1,25 @@
 import React, { FC } from "react"
 import { observer } from "mobx-react-lite"
-import { View, ViewStyle } from "react-native"
+import { ViewStyle } from "react-native"
 import { StackScreenProps } from "@react-navigation/stack"
 import { AppStackScreenProps } from "../navigators"
-import { Screen } from "../components"
 
 import { useNavigation } from "@react-navigation/native"
 import ChatWidget from '@papercups-io/chat-widget-native'
+import { useStores } from "../models"
 
 export const HomeScreen: FC<StackScreenProps<AppStackScreenProps, "Home">> = observer(function HomeScreen() {
 
-
+ const {userStore:{currentUser}} = useStores()
 
  const navigation = useNavigation();
+ if(!currentUser.ID && !currentUser.display_name && !currentUser.ID){
+  navigation.navigate('Settings')
+ }
+ 
 
   return (
-    <Screen style={root} preset="scroll">
-    <View style={$container}>
+  
     <ChatWidget
             accountId="997f9a1e-90b7-4236-b2cf-8d2d09bf0f4c"
             title="Navenu Concierge"
@@ -26,21 +29,12 @@ export const HomeScreen: FC<StackScreenProps<AppStackScreenProps, "Home">> = obs
              inbox= "e1bb6f96-0947-42e4-a10e-c1f77b6e3fee"
             baseUrl="https://app.papercups.io"
             customer={{
-              name: 'Test User',
-              email: 'test@test.com',
-              external_id: '123',
+              name: currentUser.display_name,
+              email: currentUser.user_email,
+              external_id: currentUser.ID,
             }}
           />
-    </View>
-    </Screen>
+ 
   )
 })
-const root :ViewStyle ={
 
-}
-const $container: ViewStyle = {
-  flexDirection: 'column',
-  flex: 1,
-  backgroundColor: '#FFFFFF',
-
-}
